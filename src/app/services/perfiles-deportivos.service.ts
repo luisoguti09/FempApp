9eab08bcf30706159e6ee85f709d90ec3788c2ae
Luis Gutierrez <luisoguti09@gmail.com>
2026-05-03 21:07:07 -0300

    perfiles deportivos repaired

--- src/app/services/perfiles-deportivos.service.ts
+++ src/app/services/perfiles-deportivos.service.ts
@@ -11,7 +11,7 @@ import {
 })
 export class PerfilesDeportivosService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://nodejs-production-ea14.up.railway.app/perfiles-deportivos';
+  private apiUrl = 'https://nodejs-production-ea14.up.railway.app/perfiles-deportivos';
   //private apiUrl = 'http://localhost:3000/perfiles-deportivos';
 
   getByUsuario(usuarioId: number): Observable<PerfilDeportivo[]> {
